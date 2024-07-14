d8d0b40cac67fc3c85aba9187d2b323d006f7e87
Zak Yeomanson <zakyeomanson@gmail.com>
2024-07-14 12:57:59 +0100

    Added catppuccin & toggle term plugins
    leader tt for popup toggle term. Leader t to view which key menu for other toggle term keybinds

--- lunarnvim-init.lua
+++ lunarnvim-init.lua
@@ -1,4 +1,9 @@
-
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
 lvim.transparent_window = true
 lvim.builtin.lualine.style = "default" -- or "none"
 lvim.builtin.nvimtree.active = false   -- NOTE: using neo-tree
@@ -8,15 +13,28 @@ lvim.builtin.which_key.mappings['f'] = {}
 lvim.builtin.which_key.mappings['e'] = {}
 -- Auto command to enable word wrap for Markdown files only
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown",
-    callback = function()
-        vim.opt_local.wrap = true  -- Enable word wrap
-        vim.opt_local.linebreak = true  -- Wrap lines at word boundaries
-    end,
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true      -- Enable word wrap
+    vim.opt_local.linebreak = true -- Wrap lines at word boundaries
+  end,
 })
 
 lvim.plugins = {
-{ "savq/melange-nvim"},
+  { "savq/melange-nvim" },
+  { "catppuccin/nvim" },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require('nvim-ts-autotag').setup({
+        opts = {
+          enable_close = true,          -- Auto close tags
+          enable_rename = true,         -- Auto rename pairs of tags
+          enable_close_on_slash = false -- Auto close on trailing </
+        },
+      })
+    end
+  },
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
@@ -55,7 +73,7 @@ lvim.plugins = {
     event = "BufRead",
     config = function()
       vim.cmd "highlight default link gitblame SpecialComment"
-      require("gitblame").setup { 
+      require("gitblame").setup {
         enabled = true,
         delay = 5000
       }
@@ -70,22 +88,59 @@ lvim.plugins = {
     config = function()
       local neoscroll = require('neoscroll')
       neoscroll.setup({
+        -- Default easing function used in any animation where
+        -- the `easing` argument has not been explicitly supplied
         easing = "quadratic"
       })
     end,
   },
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        --open_mapping = [[<leader>t]],
+        size = function(term)
+          if term.direction == "horizontal" then
+            return 25
+          elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+          else
+            return 20
+          end
+        end,
+        hide_numbers = true,
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = false,
+        insert_mappings = true,
+        persist_size = true,
+        direction = 'float',
+        close_on_exit = false,
+        float_opts = {
+          border = 'curved',
+          winblend = 0,
+        },
+        on_open = function(term)
+          local opts = { buffer = 0 }
+          vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], opts)
+          vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts) -- Exit insert mode in terminal using 'jk'
+          vim.keymap.set('t', '<C-w>]', '<cmd>close<CR>', opts)
+        end
+      })
+    end,
+  },
+
 
 }
 
-
-lvim.colorscheme="melange"
+lvim.colorscheme = "melange"
 
 lvim.builtin.treesitter.highlight.enabled = true
 
 -- Navigate between buffers with [b and ]b
 lvim.keys.normal_mode["]b"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["[b"] = ":BufferLineCyclePrev<CR>"
-lvim.format_on_save.enabled = false
+lvim.format_on_save.enabled = true
 
 lvim.builtin.cmp.enabled = true
 -- Import which-key register function
@@ -98,7 +153,13 @@ wk.register({
     f = { "<cmd>Telescope find_files<cr>", "Find File" },
     w = { "<cmd>Telescope live_grep<cr>", "Find Word" },
     o = { "<cmd>Telescope oldfiles<cr>", "Recently Opened Files" }
-  }
+  },
+  t = {
+    name = "Terminal", -- Group name for all terminal related keybindings
+    t = { "<cmd>ToggleTerm<cr>", "Toggle Floating Terminal" },
+    h = { "<cmd>ToggleTerm direction=horizontal<cr>", "Horizontal Split Terminal" },
+    v = { "<cmd>ToggleTerm direction=vertical<cr>", "Vertical Split Terminal" },
+  },
 }, { prefix = "<leader>" })
 wk.register({
   e = { "<cmd>Neotree toggle<CR>", "Toggle Neotree" }
